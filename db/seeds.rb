bccdaf7c08b356fd45941ef3f34b80958a2e5758
Sanju Varkey <5578483+chief10sguru@users.noreply.github.com>
2025-01-13 11:57:46 +0000

    REBOOT Rails Cookbook Add Seed Recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning the DB..."
+Recipe.destroy_all
+
+puts "Creating new recipes...."
+Recipe.create!(
+  name: "Chicken Tikka",
+  description: "Creamy & Delicious Chicken Tikka Masala", image_url: https://www.allrecipes.com/thmb/orIsAAaBiDAZYsp10qhYfB1YCIY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/5133952_indian-chicken-tikka-masala_allrecipes-2000-b955e1b914a646c4938db0a807be7c70.jpg,
+  rating: 4)
+
+Recipe.create!(
+  name: "Lamb Shank",
+  description: "Slow Cooked Leg of Lamb",
+  image_url: https://www.allrecipes.com/thmb/b5y8rHt38JtxpFqWIafQvDxNKW0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/beer-braised-lamb-shanks-photo-by-chef-john-5272240-2000-6a684dfafbe84992b693994c9db92770.jpg,
+  rating: 5)
+
+puts "Finished! Created #{Recipe.count} recipes."
